5e0472dea4824822db68ac56d9656b05829a653b
Sylvian Kimkpe <Yhawkhisses2@gmail.com>
2023-05-31 18:18:45 +0000

    controller_mode

---
+++ build/panda_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rossim/panda_ws/src/panda_arm
+# Build directory: /home/rossim/panda_ws/build/panda_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
